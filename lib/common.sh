6718c0606dbf33d29e92dfd600f454e04555cfcb
eduxatelite <e.busquets@xatelite.com>
2026-04-15 00:23:40 +0200

    Update common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -18,8 +18,8 @@ header() { echo -e "\n${BLUE}═════════════════
 
 # --- Rocky 9.7 Cloud Image ---
 ROCKY_VERSION="9.7"
-ROCKY_IMG_URL="http://ftp.madrid.xatelite.com:5005/2026/Rocky-9-6.x86_64.qcow2"
-ROCKY_IMG_NAME="Rocky-9-6.x86_64.qcow2"
+ROCKY_IMG_URL="http://ftp.madrid.xatelite.com:5005/2026/Rocky-9-7.x86_64.qcow2"
+ROCKY_IMG_NAME="Rocky-9-7.x86_64.qcow2"
 
 # --- Cloud-Init: credenciales por defecto ---
 CI_USER="root"
